f7f5fda118f9b6e3c7c4c9af859fdbc1d1b71240
Voda Tudor-Andrei <tvoda25@gmail.com>
2024-10-16 09:35:54 +0300

    JavaFx SceneBuilder Test Created dbConnection class Tested HelloController

--- src/main/java/org/example/oop_project3/HelloController.java
+++ src/main/java/org/example/oop_project3/HelloController.java
@@ -1,14 +1,27 @@
 package org.example.oop_project3;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.shape.Circle;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Circle circle;
+    private double x;
+    private double y;
+    public void up(ActionEvent e){
+        circle.setCenterY(y--);
+    }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void down(ActionEvent e){
+        circle.setCenterY(y++);
+    }
+
+    public void left(ActionEvent e){
+        circle.setCenterX(x--);
+    }
+
+    public void right(ActionEvent e){
+        circle.setCenterX(x++);
     }
 }
--- src/main/java/org/example/oop_project3/MainApp.java
+++ src/main/java/org/example/oop_project3/MainApp.java
@@ -1,5 +1,7 @@
 package org.example.oop_project3;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -11,41 +13,18 @@ import java.sql.SQLException;
 
 public class MainApp extends Application {
 
-    // MySQL database connection variables
-    private final String JDBC_URL = "jdbc:mysql://localhost:3306/movie";
-    private final String USERNAME = "root";
-    private final String PASSWORD = "";
 
     public static void main(String[] args) {
         launch(args); // Launch the JavaFX application
+
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        // Try to connect to the MySQL database
-        boolean isConnected = connectToDatabase();
-
-        // Create a simple JavaFX UI to show the connection status
-        Label label = new Label(isConnected ? "Connected to Database" : "Failed to Connect");
-        StackPane root = new StackPane(label);
-        Scene scene = new Scene(root, 300, 200);
-
-        primaryStage.setTitle("JavaFX MySQL Connection");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private boolean connectToDatabase() {
-        try {
-            // Connect to MySQL database
-            Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-            System.out.println("Connection successful!");
-            connection.close();
-            return true;
-        } catch (SQLException e) {
-            System.out.println("Error connecting to the database.");
-            e.printStackTrace();
-            return false;
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("/org/example/oop_project3/hello-view.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("JavaFX Application");
+            primaryStage.setScene(scene);
+            primaryStage.show();
         }
     }
-}
