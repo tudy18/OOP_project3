da3f60a6517e0469938fab76e7288b235ea2c00e
Voda Tudor-Andrei <tvoda25@gmail.com>
2024-10-28 15:36:00 +0200

    View layout

---
+++ src/main/java/MovieSearchController.java
@@ -0,0 +1,18 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class MovieSearchController {
+    public TextField titleField;
+    public TextField genreField;
+    public TextField dateField;
+    public ListView movieListView;
+    public ImageView movieImage;
+
+    public void handleSearch(ActionEvent actionEvent) {
+    }
+
+    public void handleReserveButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/SeatSelectionController.java
@@ -0,0 +1,9 @@
+import javafx.event.ActionEvent;
+
+public class SeatSelectionController {
+    public void handleBack(ActionEvent actionEvent) {
+    }
+
+    public void handleConfirm(ActionEvent actionEvent) {
+    }
+}
