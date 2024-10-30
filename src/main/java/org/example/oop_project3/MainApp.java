4da10fb6fc5009660ee5f289613d2e922ce9085c
Mezei Monika <mezeimonika81@gmail.com>
2024-10-30 21:47:10 +0200

    main setup

--- src/main/java/org/example/oop_project3/MainApp.java
+++ src/main/java/org/example/oop_project3/MainApp.java
@@ -5,12 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.example.oop_project3.controllers.HomeController;
+
 public class MainApp extends Application {
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-            HomeController homeController = new HomeController();
-            homeController.loadHomeView(primaryStage);
-        }
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Home");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
