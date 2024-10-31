2497a99695ec07d31c0ef312d6dc4d224a33e695
Voda Tudor-Andrei <tvoda25@gmail.com>
2024-10-31 13:24:15 +0200

    user.java

--- src/main/java/org/example/oop_project3/controllers/LoginController.java
+++ src/main/java/org/example/oop_project3/controllers/LoginController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.example.oop_project3.models.User;
 
 public class LoginController {
 @FXML
